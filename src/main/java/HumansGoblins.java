fed5487a81c2370c20e08c06b345ebf245474d56
Charles Badibanga <charlesbadibanga2000@yahoo.fr>
2023-03-14 15:56:36 -0400

    I updated the main file

--- src/main/java/HumansGoblins.java
+++ src/main/java/HumansGoblins.java
@@ -1,2 +1,30 @@
+import java.util.Random;
 public class HumansGoblins {
+
+
+    class Land {
+        private char symbol = '\u25A1';
+
+        public String toString() {
+            return String.valueOf(symbol);
+        }
+    }
+
+    class Goblin {
+        private char symbol = '\u2620';
+
+        public String toString() {
+            return String.valueOf(symbol);
+        }
+    }
+
+    class Human {
+        private char symbol = '\u2642';
+        private int health = 100;
+        private int damage = 10;
+
+        public String toString() {
+            return String.valueOf(symbol);
+        }
+    }
 }
